9b003e7cee1af481b6f3640611077cc302aa30be
conniepope <conniepope77@gmail.com>
2019-06-06 09:53:38 -0400

    table set up in html

---
+++ assets/javascript.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCZEHTeSnlmfqLUPePA2P-KEYrjWqWLf9o",
+    authDomain: "train-scheduler-edc7e.firebaseapp.com",
+    databaseURL: "https://train-scheduler-edc7e.firebaseio.com",
+    projectId: "train-scheduler-edc7e",
+    storageBucket: "",
+    messagingSenderId: "966169066890",
+    appId: "1:966169066890:web:fcab01411dc99347"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
